406a3a6a90d2baee96528cb6db815219cbb4cee0
Tofu-cube <g.b.d.r.s.p@gmail.com>
2025-12-19 19:32:52 +0800

    工具

---
+++ database.js
@@ -0,0 +1,49 @@
+// 檔案名稱：database.js
+// 這是全站共用的學生資料庫
+// 每次在 GM Mode 產生的代碼，請直接覆蓋這裡面的內容！
+
+const studentData = [
+    {
+        "id": 1,
+        "name": "艾利克斯",
+        "level": 5,
+        "avatar": "https://api.dicebear.com/7.x/avataaars/svg?seed=Alex",
+        "stats": { "單字": 80, "文法": 60, "口說": 40, "聽力": 70, "寫作": 50 }
+    },
+    {
+        "id": 2,
+        "name": "貝拉",
+        "level": 8,
+        "avatar": "https://api.dicebear.com/7.x/avataaars/svg?seed=Bella",
+        "stats": { "單字": 50, "文法": 90, "口說": 60, "聽力": 80, "寫作": 85 }
+    },
+    {
+        "id": 3,
+        "name": "查理",
+        "level": 3,
+        "avatar": "https://api.dicebear.com/7.x/avataaars/svg?seed=Charlie",
+        "stats": { "單字": 40, "文法": 40, "口說": 90, "聽力": 50, "寫作": 30 }
+    },
+    {
+        "id": 4,
+        "name": "大衛",
+        "level": 6,
+        "avatar": "https://api.dicebear.com/7.x/avataaars/svg?seed=David",
+        "stats": { "單字": 60, "文法": 70, "口說": 50, "聽力": 60, "寫作": 60 }
+    },
+    {
+        "id": 5,
+        "name": "雅雁",
+        "level": 99,
+        "avatar": "https://cdn-icons-png.freepik.com/512/435/435036.png",
+        "stats": { "單字": 100, "文法": 100, "口說": 100, "聽力": 100, "寫作": 100 }
+    },
+    {
+        "id": 6,
+        "name": "Todd",
+        "level": 7,
+        "avatar": "https://cdn-icons-png.freepik.com/512/435/435067.png",
+        "stats": { "單字": 10, "文法": 10, "口說": 10, "聽力": 10, "寫作": 10 }
+    }
+
+];
